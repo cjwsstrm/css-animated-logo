438783d3841339feede6e59d37db117dc11d7af0
CJ <cj@gokonstruct.com>
2019-04-22 17:34:59 -0600

    added fix to not have language selection be populated over and over on button click

--- script.js
+++ script.js
@@ -178,21 +178,24 @@ window.addEventListener('DOMContentLoaded', function(event) {
   var voices = [];
 
   function populateVoiceList() {
-    voices = synth.getVoices();
-  
-    for(i = 0; i < voices.length ; i++) {
-      var option = document.createElement('option');
-      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-      
-      if(voices[i].default) {
-        option.textContent += ' -- DEFAULT';
+    if (voices.length === 0) {
+      voices = synth.getVoices();
+    
+      for(i = 0; i < voices.length ; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+        
+        if(voices[i].default) {
+          option.textContent += ' -- DEFAULT';
+        }
+    
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.appendChild(option);
       }
-  
-      option.setAttribute('data-lang', voices[i].lang);
-      option.setAttribute('data-name', voices[i].name);
-      voiceSelect.appendChild(option);
     }
-  }
+
+    }
   populateVoiceList();
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
